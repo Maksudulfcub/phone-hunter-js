ea9e4fbea10b26c73b431dcc432d18a8fc33e39c
Maksudul <maaksuduldish@gmail.com>
2025-02-28 19:30:45 +0600

    Showing all data in UI

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('Phone-hunter connected');
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    displayPhones(phones);
+}
+
+const displayPhones = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure>
+        <img src="${phone.image}" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>${phone.slug}</p>
+             <div class="card-actions justify-end">
+                <button class="btn btn-primary">Show Details</button>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    });
+}
+
+loadPhone();
